4a32bd7833bffaf2e19ed71195616bccff6b90aa
fabioarujo <fabioaraujo1602@gmail.com>
2021-11-11 02:54:46 -0300

    configurando o menu

---
+++ src/helpers/createMenuObject.ts
@@ -0,0 +1,16 @@
+type MeuOptions = '' | 'all' | 'dog' | 'cat' | 'fish';
+
+export const createMenuObject = (activeMenu: MeuOptions) => {
+    let returnObject = {
+        all: false,
+        dog: false,
+        cat: false,
+        fish: false
+    }
+
+    if(activeMenu !== ''){
+        returnObject[activeMenu] = true;
+    }
+
+    return returnObject;
+}
