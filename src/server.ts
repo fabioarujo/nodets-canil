1bc101b66bedc64cc1047830cb831414c663e6f0
fabioarujo <fabioaraujo1602@gmail.com>
2021-11-12 00:14:25 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res)=>{
-    res.send('pagina nao encotrada!');
-})
+    res.render('pages/404');
+});
+
 server.listen(process.env.PORT);
